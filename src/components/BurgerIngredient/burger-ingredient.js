e9e607c437b943a3df9a0d74921b7c02a9d83d42
Alexander <sablin.irk@gmail.com>
2021-04-24 00:31:47 +0800

    Counters enabled

--- src/components/BurgerIngredient/burger-ingredient.js
+++ src/components/BurgerIngredient/burger-ingredient.js
@@ -1,4 +1,4 @@
-import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components'
 import React from 'react'
 
 import styles from './burger-ingredient.module.css'
@@ -7,6 +7,7 @@ import styles from './burger-ingredient.module.css'
 const BurgerIngredient = (props) =>{
     return (
         <div class={styles.ingridientBox}>
+            <Counter count={1} size="default" />
             <img src={props.image} alt={props.name}/>
             <p className="text text_type_main-medium">{props.price}  <CurrencyIcon type="primary" /></p>
             <p className="text text_type_main-default">{props.name}</p>
