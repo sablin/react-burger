77aea517b0671dfee7c0ae009c5a2a01899b5277
Alexander <sablin.irk@gmail.com>
2021-04-26 20:29:58 +0800

    Исправление после ревью

--- src/components/BurgerConstructor/burger-constructor.js
+++ src/components/BurgerConstructor/burger-constructor.js
@@ -6,10 +6,21 @@ const BurgerConstructor = (props) => {
     return (
         <>
         <div className={styles.burgerConstructor}>
+        <span>
+            {
+              <li  key={props.items[0]._id}><ConstructorElement
+              thumbnail={props.items[0].image_mobile}
+               text={props.items[0].name}
+               price={props.items[0].price}
+               isLocked={true}
+               type="top"
+              
+         />
+         </li>
+            }</span>
         <ul className={styles.BurgerContainer}>
-    {props.items.map((info, index) => (
-
-     <li  key={index}><ConstructorElement
+    {props.items.map((info, index) => {
+     return info.type != 'bun' ? <li  key={info._id}><ConstructorElement
      thumbnail={info.image_mobile}
       text={info.name}
       price={info.price}
@@ -17,13 +28,25 @@ const BurgerConstructor = (props) => {
       type="top"
      
 />
-</li>
-
-
-
-    ))}
+</li> :
+     false
+    }
+    
+    )}
     </ul>
-             <div className={styles.result}>
+    <span>
+            {
+              <li  key={props.items[props.items.length-1]._id}><ConstructorElement
+              thumbnail={props.items[props.items.length-1].image_mobile}
+               text={props.items[props.items.length-1].name}
+               price={props.items[props.items.length-1].price}
+               isLocked={true}
+               type="top"
+              
+         />
+         </li>
+            }</span>
+    <div className={styles.result}>
             <span className="text text_type_main-large"> 610 <CurrencyIcon type="primary" /> </span>
             <Button type="primary" size="medium">
  Оформить заказ
